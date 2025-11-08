8a7c604c36298904bf898b9dbf03e93b768c1d4b
Sahdab Ansari <sahdabansari972@gmail.com>
2025-11-08 18:31:10 +0530

    introductory demo

--- 01-intro.js
+++ 01-intro.js
@@ -1,9 +1,18 @@
-const amount = 9
+// 
+// Import the built-in 'http' module
+const http = require('http');
 
-if (amount < 10) {
-  console.log('small number')
-} else {
-  console.log('large number')
-}
+// Create a server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200; // HTTP success code
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World! This is my first Node.js server ');
+});
 
-console.log(`hey it's my first node app!!!`)
+// Define the port number
+const PORT = 3000;
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
