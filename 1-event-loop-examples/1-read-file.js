2a6c3caedcbf591e6166380d56b6531e1d3001f1
Sahdab Ansari <sahdabansari972@gmail.com>
2025-11-10 08:51:52 +0530

    event loop example in Node.js

--- 1-event-loop-examples/1-read-file.js
+++ 1-event-loop-examples/1-read-file.js
@@ -1,13 +1,26 @@
-const { readFile, writeFile } = require('fs')
+const { readFile, writeFile } = require('fs');
+const { result } = require('lodash');
 
-console.log('started a first task')
-// CHECK FILE PATH!!!!
-readFile('./content/first.txt', 'utf8', (err, result) => {
-  if (err) {
-    console.log(err)
-    return
+// console.log('started a first task')
+// // CHECK FILE PATH!!!!
+// readFile('./content/first.txt', 'utf8', (err, result) => {
+//   if (err) {
+//     console.log(err)
+//     return
+//   }
+//   console.log(result)
+//   console.log('completed first task')
+// })
+// console.log('starting next task')
+
+console.log("Started first task");
+//Check file path
+readFile("../content/first.txt", 'utf8', (err, result) =>{
+  if(err){
+    console.log(err);
+    return;
   }
-  console.log(result)
-  console.log('completed first task')
+  console.log(result);
+  console.log("Completed first task ");
 })
-console.log('starting next task')
+console.log("starting next task ");
